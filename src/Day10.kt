13b29d6eae9dbbc7775940ffad57b32edcf1f40e
Henrik Laubert <henrik.laubert@uni-bayreuth.de>
2024-12-09 09:43:31 +0100

    ADD: Solution for Day #9

---
+++ src/Day10.kt
@@ -0,0 +1,71 @@
+
+fun main() {
+
+  fun parseInput(input: List<String>): List<List<Int>> {
+    return input.map { it.split("").filter(String::isNotBlank).map(String::toInt) }
+  }
+
+  fun getStartingPoints(input: List<List<Int>>): List<Coordinates> {
+    val startingPoints = mutableListOf<Coordinates>()
+    input.forEachIndexed { i, row ->
+      row.forEachIndexed { j, it ->
+        if (it == 0) startingPoints.add(Coordinates(i, j))
+      }
+    }
+    return startingPoints
+  }
+
+  fun getElementAt(input: List<List<Int>>, pos: Coordinates): Int? {
+    return input.getOrNull(pos.first)?.getOrNull(pos.second)
+  }
+
+  fun traverse(input: List<List<Int>>, thisRef: Coordinates): List<Coordinates> {
+    val current = getElementAt(input, thisRef)
+    if (current == 9) return listOf(thisRef)
+
+    val result = mutableListOf<Coordinates>()
+
+    val neighbors = listOf(
+      thisRef.copy(first = thisRef.first - 1), // up
+      thisRef.copy(first = thisRef.first + 1), // down
+      thisRef.copy(second = thisRef.second - 1), // left
+      thisRef.copy(second = thisRef.second + 1)  // right
+    )
+
+    neighbors.forEach { neighbor ->
+      getElementAt(input, neighbor)?.let {
+        if (it == current!! + 1) {
+          result += traverse(input, neighbor)
+        }
+      }
+    }
+
+    return result
+  }
+
+  fun part1(input: List<String>): Int {
+    val temp = parseInput(input)
+    val start = getStartingPoints(temp)
+
+    return start.sumOf {
+      traverse(temp, it).toSet().size
+    }
+  }
+
+  fun part2(input: List<String>): Int {
+    val temp = parseInput(input)
+    val start = getStartingPoints(temp)
+
+    return start.sumOf {
+      traverse(temp, it).size
+    }
+  }
+
+  val testInput = readInput("Day10_test")
+  check(part1(testInput) == 36)
+  check(part2(testInput) == 81)
+
+  val input = readInput("Day10")
+  part1(input).println()
+  part2(input).println()
+}

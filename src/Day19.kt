8acbd1c1c57752f389d977371afe2d72a9f4783c
Henrik Laubert <henrik.laubert@uni-bayreuth.de>
2024-12-20 11:27:03 +0100

    ADD: Solution for Day #20

---
+++ src/Day19.kt
@@ -0,0 +1,39 @@
+
+fun main() {
+  val cache = mutableMapOf<String, Long>()
+
+  fun possibleCombinations(pattern: String, towels: List<String>): Long {
+    return cache.getOrPut(pattern) {
+      if (pattern.isBlank()) 1
+      else towels.filter { pattern.startsWith(it) }.sumOf {
+        possibleCombinations(pattern.removePrefix(it), towels)
+      }
+    }
+  }
+
+  fun part1(input: List<String>): Int {
+    val (towels, pattern) = input.divideInput { (first, second) ->
+     first.first().split(", ") to second
+    }
+
+    cache.clear()
+    return pattern.count { possibleCombinations(it, towels) > 0 }
+  }
+
+  fun part2(input: List<String>): Long {
+    val (towels, pattern) = input.divideInput { (first, second) ->
+      first.first().split(", ") to second
+    }
+
+    cache.clear()
+    return pattern.sumOf { possibleCombinations(it, towels) }
+  }
+
+  val testInput = readInput("inputs/Day19_test")
+  check(part1(testInput) == 6)
+  check(part2(testInput) == 16L)
+
+  val input = readInput("inputs/Day19")
+  part1(input).println()
+  part2(input).println()
+}
---
+++ src/Day20.kt
@@ -0,0 +1,93 @@
+import kotlin.math.abs
+
+fun main() {
+
+  fun shortestPaths(
+    start: Coordinates = Coordinates(0, 0),
+    end: Coordinates,
+    gridSize: Pair<Int,Int>,
+    obstacles: List<Coordinates>
+  ): List<List<Coordinates>> {
+    val visited = mutableMapOf(start to 0)
+    val queue = mutableListOf(listOf(start))
+    val results = mutableListOf<List<Coordinates>>()
+
+    while (queue.isNotEmpty()) {
+      val current = queue.removeFirst()
+      if (current.last() == end) {
+        results.add(current)
+        continue
+      }
+
+      Direction.cardinal.forEach {
+        val next = current.last() + it
+        val costs = current.size + 1
+
+        if (next !in obstacles && next.inBounds(gridSize) && visited.getOrDefault(next, Int.MAX_VALUE) > costs) {
+          queue.add(current + next)
+          visited[next] = costs
+        }
+      }
+    }
+
+    return results
+  }
+
+  fun Coordinates.distance(other: Coordinates): Int {
+    return abs(x - other.x) + abs(y - other.y)
+  }
+
+  fun part1(input: List<String>): Int {
+    val walls = input.getCoordinatesOf { it == '#' }.map { it.toCoordinates() }
+    val start = input.getCoordinatesOfFirst('S').toCoordinates()
+    val end = input.getCoordinatesOfFirst('E').toCoordinates()
+    val gridSize = input.gridSize()
+
+    val default = shortestPaths(start, end, gridSize, walls).first()
+
+    val shortcuts = default.flatMap { c ->
+      default.mapNotNull { end ->
+        if (c.distance(end) == 2)
+          c to end
+        else
+          null
+      }
+    }
+
+    return shortcuts.count { (start, end) ->
+      val newCost = default.subList(0, default.indexOf(start) + 1).size + default.subList(default.indexOf(end), default.size).size
+      (default.size - 1) - newCost >= 100
+    }
+  }
+
+  fun part2(input: List<String>): Int {
+    val walls = input.getCoordinatesOf { it == '#' }.map { it.toCoordinates() }
+    val start = input.getCoordinatesOfFirst('S').toCoordinates()
+    val end = input.getCoordinatesOfFirst('E').toCoordinates()
+    val gridSize = input.gridSize()
+
+    val default = shortestPaths(start, end, gridSize, walls).first()
+
+    val shortcuts = default.flatMap { c ->
+      default.mapNotNull { end ->
+        if (c.distance(end) in 2..20)
+          Triple(c, end, c.distance(end))
+        else
+          null
+      }
+    }
+
+    return shortcuts.count { (start, end, distance) ->
+      val newCost = default.subList(0, default.indexOf(start) + 1).size - 1 + default.subList(default.indexOf(end), default.size).size - 1 + distance - 1
+      (default.size - 1) - newCost >= 100
+    }
+  }
+
+  val testInput = readInput("inputs/Day20_test")
+  //check(part1(testInput).also { println(it) } == 0)
+  //check(part2(testInput).also { println(it) } == 6)
+
+  val input = readInput("inputs/Day20")
+  part1(input).println()
+  part2(input).println() //998607 to low
+}
